11e3bc9c043d7acb7f90f55c34522a9d09b95392
KamilTheKoder <kamilkazmierczak0611@gmail.com>
2023-04-07 19:36:53 +0200

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,18 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { Container } from "@mui/material";
 
 const Navbar = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
-          Bank of Kamil
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography variant="h5" component="p">
+            Bank of Kamil
+          </Typography>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
